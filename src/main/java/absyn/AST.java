c864be936c57633255d9607c1c9ba7e09eef48f1
José Romildo Malaquias <malaquias@gmail.com>
2018-01-15 13:48:41 -0200

    Add AST class

---
+++ src/main/java/absyn/AST.java
@@ -0,0 +1,14 @@
+package absyn;
+
+import io.vavr.render.ToTree;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public abstract class AST implements ToTree<String> {
+
+   @Override
+   public String toString() {
+      return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+   }
+
+}
