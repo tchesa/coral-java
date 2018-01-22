5ac5bdcfaa7d1d8051ddd7f4c90cd46c6ec99b5b
José Romildo Malaquias <malaquias@gmail.com>
2018-01-22 13:53:29 -0200

    Add semantic analysis entry point for expressions

--- src/main/java/absyn/Exp.java
+++ src/main/java/absyn/Exp.java
@@ -1,11 +1,30 @@
 package absyn;
 
 import parse.Loc;
+import types.Type;
 
 public abstract class Exp extends AST {
+
+   // Type of the expression, calculated by the semantic analyser
+   public Type type;
 
    public Exp(Loc loc) {
       super(loc);
    }
+
+   // Obtain the type of the expression as a string prefixed by the given text.
+   protected String annotateType(String text) {
+      final String theType = type == null ? "" : "\n<" + type + ">";
+      return text + theType;
+   }
+
+   // Do semantic analysis of the expression
+   public Type semantic() {
+      type = semantic_();
+      return type;
+   }
+
+   // Type check the expression. Should be defined in the concrete subclasses.
+   protected abstract Type semantic_();
 
 }
