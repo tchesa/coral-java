cc665bad637a33cad8b77215d15dced9e1a1b998
José Romildo Malaquias <malaquias@gmail.com>
2018-01-22 13:52:20 -0200

    Add type mismatch error helper

--- src/main/java/error/ErrorHelper.java
+++ src/main/java/error/ErrorHelper.java
@@ -1,6 +1,7 @@
 package error;
 
 import parse.Loc;
+import types.Type;
 
 public interface ErrorHelper {
 
@@ -18,6 +19,20 @@ public interface ErrorHelper {
 
    static FatalError fatal(String format, Object... args) {
       return new FatalError(format, args);
+   }
+
+   static CompilerError typeMismatch(Loc loc, Type found, Type... expected) {
+      final StringBuilder builder = new StringBuilder();
+      final int n = expected.length;
+      if (n > 0) {
+         builder.append(expected[0]);
+         if (n > 1) {
+            for (int i = 1; i < n - 2; i++)
+               builder.append(", ").append(expected[i]);
+            builder.append(" or ").append(expected[n - 1]);
+         }
+      }
+      return error(loc, "type mismatch: found %s but expected %s", found, builder);
    }
 
 }
