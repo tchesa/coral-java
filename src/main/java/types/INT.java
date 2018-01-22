49273ac5ca659848949152c09fd1eb9f110ea86c
José Romildo Malaquias <malaquias@gmail.com>
2018-01-22 13:51:44 -0200

    Add int type representation

---
+++ src/main/java/types/INT.java
@@ -0,0 +1,14 @@
+package types;
+
+public class INT extends Type {
+
+   public static final INT T = new INT();
+
+   private INT() {
+   }
+
+   @Override
+   public String toString() {
+      return "int";
+   }
+}
