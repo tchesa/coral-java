32ceebe891d50b958704f220a83ce84b5b466e76
José Romildo Malaquias <malaquias@gmail.com>
2018-01-22 13:54:15 -0200

    Add semantic analysis of integer literals

--- src/main/java/absyn/ExpInt.java
+++ src/main/java/absyn/ExpInt.java
@@ -2,6 +2,8 @@ package absyn;
 
 import io.vavr.collection.Tree;
 import parse.Loc;
+import types.INT;
+import types.Type;
 
 public class ExpInt extends Exp {
    public final int value;
@@ -15,4 +17,10 @@ public class ExpInt extends Exp {
    public Tree.Node<String> toTree() {
       return Tree.of("ExpInt: " + value);
    }
+
+   @Override
+   protected Type semantic_() {
+      return INT.T;
+   }
+
 }
