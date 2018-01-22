0bd1be7731e6ec1ca8c7a76df6140e82e8bc4c71
José Romildo Malaquias <malaquias@gmail.com>
2018-01-22 15:20:13 -0200

    Add semantic analysis of arithmetic expressions

--- src/main/java/absyn/ExpBin.java
+++ src/main/java/absyn/ExpBin.java
@@ -2,6 +2,11 @@ package absyn;
 
 import io.vavr.collection.Tree;
 import parse.Loc;
+import types.INT;
+import types.Type;
+
+import static error.ErrorHelper.fatal;
+import static error.ErrorHelper.typeMismatch;
 
 public class ExpBin extends Exp {
    public enum Operator {
@@ -23,6 +28,27 @@ public class ExpBin extends Exp {
 
    @Override
    public Tree.Node<String> toTree() {
-      return Tree.of("ExpBin: " + op, left.toTree(), right.toTree());
+      return Tree.of(annotateType("ExpBin: " + op), left.toTree(), right.toTree());
+   }
+
+   @Override
+   protected Type semantic_() {
+      left.semantic();
+      right.semantic();
+      switch (op) {
+         case PLUS:
+         case MINUS:
+         case TIMES:
+         case DIV:
+         case MOD:
+            if (! left.type.is(INT.T))
+               throw typeMismatch(left.loc, left.type, INT.T);
+            if (! right.type.is(INT.T))
+               throw typeMismatch(right.loc, right.type, INT.T);
+            return INT.T;
+         default:
+            // IMPLEMENTAR OPERAÇÕES RELACIONAIS E LÓGICAS
+            throw fatal("incomplete");
+      }
    }
 }
