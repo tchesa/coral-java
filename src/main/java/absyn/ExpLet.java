d455c905bc99314e6bbc899656bab36c91e160fe
José Romildo Malaquias <malaquias@gmail.com>
2018-01-29 14:23:40 -0200

    Add semantic analysis of let expression

---
+++ src/main/java/absyn/ExpLet.java
@@ -0,0 +1,40 @@
+package absyn;
+
+import env.Env;
+import env.VarEntry;
+import io.vavr.collection.List;
+import io.vavr.collection.Tree;
+import parse.Loc;
+import types.Type;
+import types.VOID;
+
+public class ExpLet extends Exp {
+   public final String variable;
+   public final Exp init;
+   public final Exp body;
+
+   public ExpLet(Loc loc, String variable, Exp init, Exp body) {
+      super(loc);
+      this.variable = variable;
+      this.init = init;
+      this.body = body;
+   }
+
+   @Override
+   public Tree.Node<String> toTree() {
+      return Tree.of(annotateType("ExpLet"),
+                     Tree.of(variable),
+                     init.toTree(),
+                     body.toTree());
+   }
+
+   @Override
+   protected Type semantic_(Env env) {
+      init.semantic(env);
+      env.venv.beginScope();
+      env.venv.put(variable, new VarEntry(init.type));
+      body.semantic(env);
+      env.venv.endScope();
+      return body.type;
+   }
+}
