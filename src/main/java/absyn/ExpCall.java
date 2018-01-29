12ab6facb01e51e6b107bc33c49526667ab6aa36
José Romildo Malaquias <malaquias@gmail.com>
2018-01-29 15:27:18 -0200

    Add semantic analysis of function calls (part 2)

--- src/main/java/absyn/ExpCall.java
+++ src/main/java/absyn/ExpCall.java
@@ -1,11 +1,14 @@
 package absyn;
 
+import env.Entry;
 import env.Env;
+import env.FunEntry;
 import io.vavr.collection.List;
 import io.vavr.collection.Tree;
 import parse.Loc;
 import types.Type;
-import types.VOID;
+
+import static error.ErrorHelper.*;
 
 public class ExpCall extends Exp {
    public final String function;
@@ -25,6 +28,22 @@ public class ExpCall extends Exp {
 
    @Override
    protected Type semantic_(Env env) {
-      
+      Entry entry = env.venv.get(function);
+      if (entry == null)
+         throw undefined(loc, "function", function);
+      if (!(entry instanceof FunEntry))
+         throw notAFunction(loc, function);
+      FunEntry fentry = (FunEntry) entry;
+      if (arguments.size() < fentry.formals.size())
+         throw tooFewArguments(loc, function);
+      if (arguments.size() > fentry.formals.size())
+         throw tooMuchArguments(loc, function);
+      fentry.formals.zipWith(arguments,
+                             (f, a) -> {
+                                if (!a.semantic(env).is(f))
+                                   throw typeMismatch(a.loc, a.type, f);
+                                return 0;
+                             });
+      return fentry.result;
    }
 }
